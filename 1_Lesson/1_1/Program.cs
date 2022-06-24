44ad75486b7221b721c9012f8e5313155e4a00e6
sweeties <frosiya@mail.ru>
2022-06-24 17:41:59 +0300

    Два числа

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, all people in the world");
+﻿//string num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+if (numA * numA == numB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");  
+}
+//Console.WriteLine($"{num}^2: {num * num}");
