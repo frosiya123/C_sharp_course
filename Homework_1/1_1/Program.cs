ce1f2c13baa5d540cd82b17eebcf222dd918bfd4
sweeties <frosiya@mail.ru>
2022-06-26 20:10:52 +0300

    Максимум 2 чисел

---
+++ Homework_1/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int max = x;
+
+if(x > max) max = x;
+if(y > max) max = y;
+
+Console.Write("max = ");
+Console.WriteLine(max);
