7a3bdcbd5a10ca356a1f10d16868654b3f2ddc4e
sweeties <frosiya@mail.ru>
2022-06-27 10:55:06 +0300

    Последнее число

---
+++ Lesson_2/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа, которая принимает на вход трехзначное число и на выходе
+//показывает последнюю цифру этого числа
+
+int TakeNum(int num)
+ { 
+    return num % 10;
+ } 
+ Console.WriteLine(TakeNum(678)); 
+ Console.WriteLine(TakeNum(678565565));  
