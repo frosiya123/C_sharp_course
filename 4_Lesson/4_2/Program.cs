bc70d1b15b8c2ce8fbdf036058e2e03dd40e2e0d
sweeties <frosiya@mail.ru>
2022-07-04 10:29:29 +0300

    Задача 2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// 1. Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе. 
+
+int CountNum(int num) 
+{ 
+    string result = num.ToString(); 
+    return result.Length; 
+} 
+Console.WriteLine(CountNum(456)); 
