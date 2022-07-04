5527d0f67bf1dcdc248d74ecab955d26050cff15
sweeties <frosiya@mail.ru>
2022-07-04 17:38:00 +0300

    Задача 4 более сложное решение

---
+++ 4_Lesson/4_4_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// 3. Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке. 
+
+
+void ArrayFill(int[] array)
+	{
+	    for (int i = 0; i < array.Length; i++)
+	    {
+	        array[i] = new Random().Next(0, 11);
+	    }
+	}
+	
+	void ArrayPrint(int[] array)
+	{
+	    for (int i = 0; i < array.Length; i++)
+	    {
+	        Console.Write($"{array[i]} ");
+	    }
+	}
+	
+	int[] array = new int[8];
+	ArrayFill(array);
+	ArrayPrint(array);
