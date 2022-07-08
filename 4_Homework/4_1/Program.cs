a5a0a0ae19fe2d14b7a4f9bd41bf4fb060e9541f
sweeties <frosiya@mail.ru>
2022-07-08 14:21:52 +0300

    Задача 3

--- 4_Homework/4_1/Program.cs
+++ 4_Homework/4_1/Program.cs
@@ -1,4 +1,20 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
 //и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
+
+//int Numb(int numA, int numB)
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int Numb = numA;
+{
+    int res = numA;
+    for (int i = 2; i <= numB; i++)
+    {
+    res *= numA;
+    }
+    return res;
+}
+
+Console.WriteLine(Numb);
+//я хочу вводить данные через консоль. Не получается вывод
---
+++ 4_Homework/4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+//int[] numbers = { 1, 2, 3, 4, 5 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.Write($" {numbers[i]}");
+//}
+void FillArray(int[] number)
+{
+    int length = number.Length; 
+    int index = 0;
+    while (index < length)
+    {
+         number[index] = new Random().Next(1, 100);      
+        index++;
+    }
+
+}
+void PrintArray(int[] num)
+{
+    int count = num.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($" {num[position]}");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
