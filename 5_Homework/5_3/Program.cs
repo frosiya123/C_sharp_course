d1204546bf6b248c69f3a35900427c8aff1f299e
sweeties <frosiya@mail.ru>
2022-07-10 11:53:14 +0300

    задача 2

---
+++ 5_Homework/5_3/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между
+// максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+void Print(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] MassNums(int size)
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-10, 10);
+    }
+    return arr;
+}
+double spliceMinMax(double[] array){
+    double min = array[0];
+    double max = array[0];
+
+    for( int i =0; i< array.Length;i++){
+        if( array[i] < min ){
+            min = array[i];
+        }
+        if( array[i] > max ){
+            max = array[i];
+        }
+    }
+    return max - min;
+}
+
+double[] arr_1 = MassNums(10);
+Print(arr_1);
+Console.WriteLine(spliceMinMax(arr_1));
