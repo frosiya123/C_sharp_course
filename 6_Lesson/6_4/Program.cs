302675f1d13be3218f9d46e28cdf2e8415f6ea0f
sweeties <frosiya@mail.ru>
2022-07-11 11:23:49 +0300

    Задача 4 Фибоначи

---
+++ 6_Lesson/6_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1. 
+
+void FibNum(int num)
+{
+    int numA = 0;
+    int numB = 1;
+    for (int i = 0; i < num; i++){
+        Console.Write($"{numA} ");
+        (numA, numB) = (numB, numA + numB);
+    }
+}
+FibNum(5);
