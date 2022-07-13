b04af2066787ceb7db4152c7d85e9ef6b381c7f6
sweeties <frosiya@mail.ru>
2022-07-13 15:43:16 +0300

    Задача 1

--- 6_Homework/6_1/Program.cs
+++ 6_Homework/6_1/Program.cs
@@ -2,16 +2,22 @@
 //больше 0 ввел пользователь
 //0, 7, 8, -2, -2 -> 2
 //-1, -7, 567, 89, 223 ->3
-// Console.Write("Введите числа(через пробел): ");
-// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-// int count = 0;
+Console.Write("Введите числа(через пробел): ");
+string[] arr = Console.ReadLine().Split();
+int count = 0;
  
-// for (int i = 0; i < arr.Length; i++)
-// {
-//     if (arr[i] > 0)
-//     {
-//         count++;
-//     }
-// }
+for (int i = 0; i < arr.Length; i++)
+{
+    int number;
+    if( !int.TryParse(arr[i],out number) )
+    {
+        Console.WriteLine($"{i+1} Символ {arr[i]} не является целым числом");
+        continue;
+    }
+    if (number > 0)
+    {
+        count++;
+    }
+}
  
-// Console.WriteLine($"Больше нуля: {count}");
+Console.WriteLine($"Больше нуля: {count}");
