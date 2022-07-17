f25f4f59fb8b7e401ef9a87b396551dbdf34051e
sweeties <frosiya@mail.ru>
2022-07-17 11:36:32 +0300

    Задача 1

---
+++ 7_Lesson/7_1/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 1: Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.
+//m = 3, n = 4.
+//1 4 8 19
+//5 -2 33 -2
+//77 3 8 1
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        } 
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {   
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }        
+    }
+    return arr;
+}
+
+
+Console.WriteLine("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] arr_1 = MassNums(row, column, 1, 101);
+Print(arr_1);
