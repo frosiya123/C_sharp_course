8368b50f37c87a4746eaa938bb8b5aad992feb37
sweeties <frosiya@mail.ru>
2022-07-23 19:31:52 +0300

    Задача 1

---
+++ 9_Homework/9_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int PowerNumb(int numA, int numB){
+    if(numB < numA) return 0;
+    return PowerNumb(numA, numB -1) + numB;
+}
+Console. Write(PowerNumb(1, 15));
